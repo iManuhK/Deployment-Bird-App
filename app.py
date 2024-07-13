74648d8c7e7d2b569c170765daeeca1c3fc072bd
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-07-13 14:05:30 +0300

    adds routes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -23,5 +23,25 @@ class Birds(Resource):
     def get(self):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
+    
+    def post(self):
+        new_post = Bird(
+            name = request.json.get['name'],
+            species = request.json.get['species'],
+        )
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        new_post_dict = new_post.to_dict()
+
+        return make_response(new_post_dict, 201)
 
 api.add_resource(Birds, '/birds')
+
+class BirdByID(Resource):
+    def get(self, id):
+        bird = Bird.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(bird), 200)
+
+api.add_resource(BirdByID, '/birds/<int:id>')
